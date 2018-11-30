d044a9e78d9416ae3a1e2184470e7a28ddd8024d
Andre Garcia <andregarcia@dcc.ufmg.br>
2018-11-30 18:03:16 -0200

    arrumando aeds

--- AEDS_II/TP0/analises.c
+++ AEDS_II/TP0/analises.c
@@ -22,3 +22,34 @@ void permutaNumeros (int *r , int *n){
   printf("%d %d\n", i+1 , *n );
   }
 }
+
+//função de gerar a permutação alterar para voltar ao normal
+int *permutaSemRep(int a, int *v, city *Map, FILE* saida , FILE* saida1)
+{
+	int i=1, j, k, z;
+
+	while (i >= 0)
+	{
+		z = 1;
+		i = a-1;
+		v[i]++;
+		while (i >= 0 && v[i] > a)
+		{
+			v[i] = 1;
+			i--;
+			if (i >= 0) v[i]++;
+
+		}
+	  for (j=0; j<a-1; j++)
+		  for (k=j+1; k<a; k++)
+		    if (v[k] == v[j])
+        {
+          z=0;
+          break;
+        }
+
+
+    	if (z) prtvet(a, v, Map, saida , saida1);
+	}
+  return 0;
+}
--- AEDS_II/TP0/main.c
+++ AEDS_II/TP0/main.c
@@ -4,9 +4,13 @@
 
 int main()
 {
-	num_in numeros;
+	// declara a struct desenvolvida para o trabalho
+	num_in numeros; //struct numeros repetição e sequência
+	//Obetendo os  numeros e armazenando na estrutura de dados
 	scanf("%d %d", &numeros.rep , &numeros.seq);
+	//Testa se os valores inseridos estão dentro dos parametros do problema
 	testaNumeros(&numeros.rep , &numeros.seq);
+	//Faz a permutação 
 	permutaNumeros(&numeros.rep , &numeros.seq);
 	return 0;
 }
